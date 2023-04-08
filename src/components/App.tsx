23014ac8faa26e57efec4018fef8764800ac9b2f
uchida-yu <yu3shabadaba+github@gmail.com>
2023-04-08 12:55:59 +0900

    環境構築

---
+++ src/components/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import AppStyle from '@/styles/App.module.scss';
+
+function App() {
+  return <div className={AppStyle.App}>foo</div>;
+}
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,50 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import sassDts from 'vite-plugin-sass-dts';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: 'src',
+  publicDir: '../public',
+  build: {
+    outDir: '../dist',
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles" as common;`,
+        importer(...args) {
+          if (args[0] !== '@/styles') {
+            return undefined;
+          }
+          return {
+            file: `${path.resolve(__dirname, './src/styles')}`,
+          };
+        },
+      },
+    },
+  },
+  plugins: [
+    react(),
+    sassDts({
+      enabledMode: ['development', 'production'],
+      global: {
+        generate: true,
+        outFile: path.resolve(__dirname, './src/styles/style.d.ts'),
+      },
+    }),
+    checker({
+      typescript: true,
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+});
